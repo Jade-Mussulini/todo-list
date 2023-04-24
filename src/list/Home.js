900ed2d493bc39472d4e83e1be1babb0d10555f9
Marcos Henrique de Souza Prado <you@example.com>
2023-04-24 13:30:43 -0300

    first commit

---
+++ src/list/Home.js
@@ -0,0 +1,17 @@
+import "./Home.css";
+function Home() {
+    return (
+        <div className="araujo">
+            <div className="afazeres-lista">
+                <div className="elementos">
+                    <input 
+                    placeholder="INSIRA UMA NOVA TAREFA..." 
+                    className="pesquisar"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
